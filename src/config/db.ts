be8d57f7d39f57e8f3256b25fe40fbde257bb557
lindo-semetsi-hub <lindosemetsi@gmail.com>
2026-02-24 12:53:24 +0200

    database neon connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,18 +3,31 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("DB FILE LOADED");
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false  //for Neon cloud
-  }
+    rejectUnauthorized: false
+  },
+
+  connectionTimeoutMillis: 5000
 });
 
 export const connectDB = async () => {
   try {
-    await pool.query("SELECT 1");
+
+    console.log("Trying DB query...");
+
+    const result = await pool.query("SELECT NOW()");
+
     console.log("PostgreSQL Connected");
+    console.log(result.rows[0]);
+
   } catch (error) {
-    console.error("DB Connection Failed", error);
+
+    console.log("DATABASE ERROR:");
+    console.log(error);
+
   }
 };
