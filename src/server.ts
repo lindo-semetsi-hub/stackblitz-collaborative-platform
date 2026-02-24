1ff959661b8201a3ba83eaafe9167290d7e52294
lindo-semetsi-hub <lindosemetsi@gmail.com>
2026-02-24 12:48:59 +0200

    serrver

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,28 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+console.log("SERVER FILE STARTED ");
+
 import app from "./app";
-
 import { connectDB } from "./config/db";
-
 
 const PORT = process.env.PORT || 5000;
 
 console.log("PORT:", process.env.PORT);
 console.log("DATABASE:", process.env.DATABASE_URL ? "Loaded" : "Missing");
 
-
-
 const startServer = async () => {
+
+  console.log("Connecting DB...");
+
   await connectDB();
 
+  console.log("Starting Express...");
+
   app.listen(PORT, () => {
-    console.log(` Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
+
 };
 
 startServer();
